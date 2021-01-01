bee6b35e2ba3aa2548c29b8ec7b2145f53f151f4
Tom Grigg <tomegrigg@gmail.com>
2020-12-31 18:46:52 -0800

    shrink

--- kernel/shared/src/main/scala/cats/effect/kernel/SyncTG.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/SyncTG.scala
@@ -16,24 +16,10 @@
 
 package cats.effect.kernel
 
-import cats.{Applicative, Defer, Monoid, Semigroup}
+import cats.{Monoid, Semigroup}
 import cats.data.{EitherT, IorT, Kleisli, OptionT, ReaderWriterStateT, StateT, WriterT}
 
-trait SyncTG[F[_]] extends MonadCancel[F, Throwable] with Clock[F] with Defer[F] {
-
-  private[this] val Delay = SyncTG.Type.Delay
-  private[this] val Blocking = SyncTG.Type.Blocking
-  private[this] val InterruptibleOnce = SyncTG.Type.InterruptibleOnce
-  private[this] val InterruptibleMany = SyncTG.Type.InterruptibleMany
-
-  override def applicative: Applicative[F] = this
-
-  def delay[A](thunk: => A): F[A] = ???
-  def defer[A](thunk: => F[A]): F[A] = ???
-  def blocking[A](thunk: => A): F[A] = ???
-  def interruptible[A](many: Boolean)(thunk: => A): F[A] = ???
-  def suspend[A](hint: SyncTG.Type)(thunk: => A): F[A]
-}
+trait SyncTG[F[_]]
 
 object SyncTG {
   implicit def syncForOptionT[F[_]](implicit F0: SyncTG[F]): SyncTG[OptionT[F, *]] = ???
@@ -43,14 +29,5 @@ object SyncTG {
   implicit def syncForIorT[F[_], L](implicit F0: SyncTG[F], L0: Semigroup[L]): SyncTG[IorT[F, L, *]] = ???
   implicit def syncForKleisli[F[_], R](implicit F0: SyncTG[F]): SyncTG[Kleisli[F, R, *]] = ???
   implicit def syncForReaderWriterStateT[F[_], R, L, S](implicit F0: SyncTG[F], L0: Monoid[L]): SyncTG[ReaderWriterStateT[F, R, L, S, *]] = ???
-
-  sealed trait Type extends Product with Serializable
-
-  object Type {
-    case object Delay extends Type
-    case object Blocking extends Type
-    case object InterruptibleOnce extends Type
-    case object InterruptibleMany extends Type
-  }
 }
 
