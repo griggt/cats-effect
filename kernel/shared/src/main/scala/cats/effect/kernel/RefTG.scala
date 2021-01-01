6c50fb85878306d640a7d260e2c20ef6ccc6b0e0
Tom Grigg <tomegrigg@gmail.com>
2020-12-31 18:34:59 -0800

    shrink

--- kernel/shared/src/main/scala/cats/effect/kernel/RefTG.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/RefTG.scala
@@ -18,42 +18,10 @@ package cats
 package effect
 package kernel
 
-import java.util.concurrent.atomic.{AtomicBoolean, AtomicReference}
-
-import cats.data.State
-import cats.instances.function._
-import cats.instances.tuple._
-import cats.syntax.bifunctor._
-import cats.syntax.functor._
-
-abstract class RefTG[F[_], A] {
-
-  def get: F[A]
-  def set(a: A): F[Unit]
-
-  def getAndUpdate(f: A => A): F[A] = ???
-  def getAndSet(a: A): F[A] = ???
-  def updateAndGet(f: A => A): F[A] = ???
-
-  def access: F[(A, A => F[Boolean])]
-  def tryUpdate(f: A => A): F[Boolean]
-  def tryModify[B](f: A => (A, B)): F[Option[B]]
-  def update(f: A => A): F[Unit]
-  def modify[B](f: A => (A, B)): F[B]
-  def tryModifyState[B](state: State[A, B]): F[Option[B]]
-  def modifyState[B](state: State[A, B]): F[B]
-
-  def mapK[G[_]](f: F ~> G)(implicit F: Functor[F]): RefTG[G, A] = ???
-}
+abstract class RefTG[F[_], A]
 
 object RefTG {
-
-  @annotation.implicitNotFound(
-    "Cannot find an instance for Ref.Make. Add implicit evidence of Concurrent[${F}, _] or Sync[${F}] to scope to automatically derive one.")
-  trait Make[F[_]] {
-    def refOf[A](a: A): F[RefTG[F, A]]
-  }
-
+  trait Make[F[_]]
   object Make extends MakeInstances
 
   private[kernel] trait MakeInstances extends MakeLowPriorityInstances {
@@ -64,17 +32,7 @@ object RefTG {
     implicit def syncInstance[F[_]](implicit F: SyncTG[F]): Make[F] = ???
   }
 
-  def apply[F[_]](implicit mk: Make[F]): ApplyBuilders[F] = ???
   def of[F[_], A](a: A)(implicit mk: Make[F]): F[RefTG[F, A]] = ???
-  def copyOf[F[_]: Make: FlatMap, A](source: RefTG[F, A]): F[RefTG[F, A]] = ???
-  def ofEffect[F[_]: Make: FlatMap, A](fa: F[A]): F[RefTG[F, A]] = ???
-  def unsafe[F[_], A](a: A)(implicit F: SyncTG[F]): RefTG[F, A] = ???
-  def in[F[_], G[_], A](a: A)(implicit F: SyncTG[F], G: SyncTG[G]): F[RefTG[G, A]] = ???
-  def lens[F[_], A, B <: AnyRef](ref: RefTG[F, A])(get: A => B, set: A => B => A)(implicit F: SyncTG[F]): RefTG[F, B] = ???
 
-  final class ApplyBuilders[F[_]](val mk: Make[F]) extends AnyVal {
-    def of[A](a: A): F[RefTG[F, A]] = ???
-  }
-
-  implicit def catsInvariantForRef[F[_]: Functor]: Invariant[RefTG[F, *]] = ???
+  //implicit def catsInvariantForRef[F[_]: Functor]: Invariant[RefTG[F, *]] = ???
 }
