121ac6a0a6129542e6e031b06733013afc68c750
Tom Grigg <tomegrigg@gmail.com>
2020-12-31 17:33:26 -0800

    shrink

--- kernel/shared/src/main/scala/cats/effect/kernel/SyncTG.scala
+++ kernel/shared/src/main/scala/cats/effect/kernel/SyncTG.scala
@@ -28,146 +28,21 @@ trait SyncTG[F[_]] extends MonadCancel[F, Throwable] with Clock[F] with Defer[F]
 
   override def applicative: Applicative[F] = this
 
-  def delay[A](thunk: => A): F[A] =
-    suspend(Delay)(thunk)
-
-  def defer[A](thunk: => F[A]): F[A] =
-    flatMap(delay(thunk))(x => x)
-
-  def blocking[A](thunk: => A): F[A] =
-    suspend(Blocking)(thunk)
-
-  def interruptible[A](many: Boolean)(thunk: => A): F[A] =
-    suspend(if (many) InterruptibleOnce else InterruptibleMany)(thunk)
-
+  def delay[A](thunk: => A): F[A] = ???
+  def defer[A](thunk: => F[A]): F[A] = ???
+  def blocking[A](thunk: => A): F[A] = ???
+  def interruptible[A](many: Boolean)(thunk: => A): F[A] = ???
   def suspend[A](hint: SyncTG.Type)(thunk: => A): F[A]
 }
 
 object SyncTG {
-  def apply[F[_]](implicit F: SyncTG[F]): F.type = F
-
-  implicit def syncForOptionT[F[_]](implicit F0: SyncTG[F]): SyncTG[OptionT[F, *]] =
-    new OptionTSync[F] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-    }
-
-  implicit def syncForEitherT[F[_], E](implicit F0: SyncTG[F]): SyncTG[EitherT[F, E, *]] =
-    new EitherTSync[F, E] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-    }
-
-  implicit def syncForStateT[F[_], S](implicit F0: SyncTG[F]): SyncTG[StateT[F, S, *]] =
-    new StateTSync[F, S] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-    }
-
-  implicit def syncForWriterT[F[_], L](implicit F0: SyncTG[F], L0: Monoid[L]): SyncTG[WriterT[F, L, *]] =
-    new WriterTSync[F, L] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-      implicit def L: Monoid[L] = L0
-    }
-
-  implicit def syncForIorT[F[_], L](implicit F0: SyncTG[F], L0: Semigroup[L]): SyncTG[IorT[F, L, *]] =
-    new IorTSync[F, L] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-      implicit def L: Semigroup[L] = L0
-    }
-
-  implicit def syncForKleisli[F[_], R](implicit F0: SyncTG[F]): SyncTG[Kleisli[F, R, *]] =
-    new KleisliSync[F, R] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit def F: SyncTG[F] = F0
-    }
-
-  implicit def syncForReaderWriterStateT[F[_], R, L, S](implicit F0: SyncTG[F], L0: Monoid[L]): SyncTG[ReaderWriterStateT[F, R, L, S, *]] =
-    new ReaderWriterStateTSync[F, R, L, S] {
-      def rootCancelScope = F0.rootCancelScope
-      implicit override def F: SyncTG[F] = F0
-      implicit override def L: Monoid[L] = L0
-    }
-
-  private[effect] trait OptionTSync[F[_]]
-      extends SyncTG[OptionT[F, *]]
-      with MonadCancel.OptionTMonadCancel[F, Throwable]
-      with Clock.OptionTClock[F] {
-
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): OptionT[F, A] =
-      OptionT.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait EitherTSync[F[_], E]
-      extends SyncTG[EitherT[F, E, *]]
-      with MonadCancel.EitherTMonadCancel[F, E, Throwable]
-      with Clock.EitherTClock[F, E] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): EitherT[F, E, A] =
-      EitherT.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait StateTSync[F[_], S]
-      extends SyncTG[StateT[F, S, *]]
-      with MonadCancel.StateTMonadCancel[F, S, Throwable]
-      with Clock.StateTClock[F, S] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): StateT[F, S, A] =
-      StateT.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait WriterTSync[F[_], S]
-      extends SyncTG[WriterT[F, S, *]]
-      with MonadCancel.WriterTMonadCancel[F, S, Throwable]
-      with Clock.WriterTClock[F, S] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): WriterT[F, S, A] =
-      WriterT.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait IorTSync[F[_], L]
-      extends SyncTG[IorT[F, L, *]]
-      with MonadCancel.IorTMonadCancel[F, L, Throwable]
-      with Clock.IorTClock[F, L] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): IorT[F, L, A] =
-      IorT.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait KleisliSync[F[_], R]
-      extends SyncTG[Kleisli[F, R, *]]
-      with MonadCancel.KleisliMonadCancel[F, R, Throwable]
-      with Clock.KleisliClock[F, R] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): Kleisli[F, R, A] =
-      Kleisli.liftF(F.suspend(hint)(thunk))
-  }
-
-  private[effect] trait ReaderWriterStateTSync[F[_], R, L, S]
-      extends SyncTG[ReaderWriterStateT[F, R, L, S, *]]
-      with MonadCancel.ReaderWriterStateTMonadCancel[F, R, L, S, Throwable]
-      with Clock.ReaderWriterStateTClock[F, R, L, S] {
-    implicit protected def F: SyncTG[F]
-    protected def C = F
-
-    def suspend[A](hint: Type)(thunk: => A): ReaderWriterStateT[F, R, L, S, A] =
-      ReaderWriterStateT.liftF(F.suspend(hint)(thunk))
-  }
+  implicit def syncForOptionT[F[_]](implicit F0: SyncTG[F]): SyncTG[OptionT[F, *]] = ???
+  implicit def syncForEitherT[F[_], E](implicit F0: SyncTG[F]): SyncTG[EitherT[F, E, *]] = ???
+  implicit def syncForStateT[F[_], S](implicit F0: SyncTG[F]): SyncTG[StateT[F, S, *]] = ???
+  implicit def syncForWriterT[F[_], L](implicit F0: SyncTG[F], L0: Monoid[L]): SyncTG[WriterT[F, L, *]] = ???
+  implicit def syncForIorT[F[_], L](implicit F0: SyncTG[F], L0: Semigroup[L]): SyncTG[IorT[F, L, *]] = ???
+  implicit def syncForKleisli[F[_], R](implicit F0: SyncTG[F]): SyncTG[Kleisli[F, R, *]] = ???
+  implicit def syncForReaderWriterStateT[F[_], R, L, S](implicit F0: SyncTG[F], L0: Monoid[L]): SyncTG[ReaderWriterStateT[F, R, L, S, *]] = ???
 
   sealed trait Type extends Product with Serializable
 
